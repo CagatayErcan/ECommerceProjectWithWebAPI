0be012f38379cf85e018331366fb448f4a58949e
user <user@DESKTOP-4D8S7CG>
2023-07-18 00:24:50 +0300

    Add project files.

---
+++ WebAPICoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPICoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
